4d532ab797058e6b47a5598c855059c63680733a
Hemanth vasu <hemanthvasu41468@gmail.com>
2026-04-24 19:29:23 +0530

    day 1 Max_Consicutive_ones

---
+++ Max_Consicutive_ones.java
@@ -0,0 +1,31 @@
+public class Max_Consicutive_ones {
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,0,1,1,1};
+
+        int result = solution(arr);
+
+        System.out.println("Max consecutive ones: " + result);
+    }
+
+    public static int solution(int[] arr) {
+        int max=0;
+
+        int count=0;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]==1)
+            {
+                count++;
+                max=Math.max(max,count);
+            }
+            if(arr[i]!=1)
+            {
+                count=0;
+            }
+        }
+
+        return max;
+    }
+}
