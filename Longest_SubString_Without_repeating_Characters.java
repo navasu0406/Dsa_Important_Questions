f5d60eaf2e582b4c980e00f4cfabfb520c198623
Hemanth vasu <hemanthvasu41468@gmail.com>
2026-04-28 17:53:29 +0530

    day 5

--- Longest_SubString_Without_repeating_Characters.java
+++ Longest_SubString_Without_repeating_Characters.java
@@ -14,17 +14,16 @@ public class Longest_SubString_Without_repeating_Characters {
 
         int l=0;
         int max=0;
-        for(int i=0; i<s.length(); i++)
-        {
-            char c=s.charAt(i);
 
+        for(int r=0; r<s.length(); r++)
+        {
+            char c=s.charAt(r);
             if(map.containsKey(c))
             {
                 l=Math.max(l,map.get(c)+1);
             }
-            map.put(c,i);
-
-            max=Math.max(max,i-l+1);
+            map.put(c,r);
+            max=Math.max(max,r-l+1);
         }
 
         return max;
